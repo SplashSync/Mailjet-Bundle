ec76c1ca92f3596a97b30fbe5ff7821051ea85ca
BadPixxel <eshop.bpaquier@gmail.com>
2026-03-04 14:19:32 +0100

    Refactor ThirdParty PropertiesTrait to use PropertiesManager for property handling

--- src/Objects/ThirdParty/PropertiesTrait.php
+++ src/Objects/ThirdParty/PropertiesTrait.php
@@ -15,8 +15,11 @@
 
 namespace Splash\Connectors\Mailjet\Objects\ThirdParty;
 
-use DateTime;
 use Exception;
+use Splash\Connectors\Mailjet\DataTransformers\PropertyTransformer;
+use Splash\Connectors\Mailjet\Models\Api\Contact;
+use Splash\OpenApi\Dictionary\ExtendedActionsTypes;
+use Splash\OpenApi\Models\Mutation;
 use stdClass;
 
 /**
@@ -25,93 +28,21 @@ use stdClass;
 trait PropertiesTrait
 {
     /**
-     * Collection of Known Attributes Names with Spacial Mapping
-     *
-     * This Collection is Public to Allow External Additions
-     *
-     * @var array<string, string[]>
-     */
-    public static array $knowAttributes = array(
-        "firstname" => array("http://schema.org/Person", "familyName"),
-        "lastname" => array("http://schema.org/Person", "givenName"),
-    );
-
-    /**
-     * Storage for Members Properties
-     *
-     * @var array
-     */
-    protected array $contactData = array();
-
-    /**
-     * Base Attributes Metadata Item Name
-     *
-     * @var string
-     */
-    private static string $baseProp = "http://meta.schema.org/additionalType";
-
-    /**
-     * Attributes Type <> Splash Type Mapping
-     *
-     * @var array<string, string>
-     */
-    private static array $attrType = array(
-        "str" => SPL_T_VARCHAR,
-        "int" => SPL_T_INT,
-        "float" => SPL_T_DOUBLE,
-        "bool" => SPL_T_BOOL,
-        "datetime" => SPL_T_DATETIME,
-    );
-
-    /**
-     * @var null|array
-     */
-    private ?array $attrCache;
-
-    /**
      * Build Fields using FieldFactory
      *
      * @return void
      */
     protected function buildPropertiesFields(): void
     {
-        //====================================================================//
-        // Safety Check => Attributes Are Loaded
-        $attributes = $this->getParameter("MembersAttributes");
-        if (empty($attributes) || !is_iterable($attributes)) {
-            return;
-        }
-        //====================================================================//
-        // Create Attributes Fields
-        $factory = $this->fieldsFactory();
-        // @codingStandardsIgnoreStart
-        foreach ($attributes as $attr) {
-            //====================================================================//
-            // Add Attribute to Fields
-            $factory
-                ->create(self::toSplashType($attr))
-                ->identifier(strtolower($attr->Name))
-                ->name($attr->Name)
-                ->group("Attributes")
-            ;
-            //====================================================================//
-            // Add Attribute MicroData
-            $attrCode = strtolower($attr->Name);
-            if (isset(self::$knowAttributes[$attrCode])) {
-                $factory->microData(
-                    self::$knowAttributes[$attrCode][0],
-                    self::$knowAttributes[$attrCode][1]
-                );
-
-                continue;
-            }
-            $factory->microData(self::$baseProp, strtolower($attr->Name));
-        }
-        // @codingStandardsIgnoreEnd
+        $this->connector
+            ->getLocator()
+            ->getPropertiesManager()
+            ->buildPropertiesFields($this->fieldsFactory())
+        ;
     }
 
     /**
-     * Read requested Field
+     * Read Requested Field
      *
      * @param string $key       Input List Key
      * @param string $fieldName Field Identifier / Name
@@ -120,19 +51,21 @@ trait PropertiesTrait
      *
      * @return void
      */
-    protected function getAttributesFields(string $key, string $fieldName): void
+    protected function getPropertiesFields(string $key, string $fieldName): void
     {
         //====================================================================//
-        // Field is not an Attribute
-        $attr = $this->isAttribute($fieldName);
-        if (is_null($attr) || !isset($this->contactData)) {
+        // Field is not a Property
+        if (!$attr = $this->isProperty($fieldName)) {
             return;
         }
         //====================================================================//
-        // Extract Attribute Value
-        // @codingStandardsIgnoreStart
-        $this->out[$fieldName] = $this->getAttributeValue($attr->Name, $attr->Datatype);
-        // @codingStandardsIgnoreEnd
+        // Ensure Properties are Loaded
+        $manager = $this->connector->getLocator()->getPropertiesManager();
+        $manager->loadContactProperties($this->object);
+        //====================================================================//
+        // Read & Transform Property Value
+        $rawValue = $this->object->properties[$fieldName] ?? null;
+        $this->out[$fieldName] = PropertyTransformer::toSplash($attr, $rawValue);
         //====================================================================//
         // Clear Key Flag
         unset($this->in[$key]);
@@ -141,182 +74,68 @@ trait PropertiesTrait
     /**
      * Write Given Fields
      *
-     * @param string      $fieldName Field Identifier / Name
-     * @param null|string $fieldData Field Data
-     *
-     * @throws Exception
+     * @param string                     $fieldName Field Identifier / Name
+     * @param null|bool|float|int|string $fieldData Field Data
      *
      * @return void
      */
-    protected function setAttributesFields(string $fieldName, ?string $fieldData): void
+    protected function setPropertiesFields(string $fieldName, null|bool|string|float|int $fieldData): void
     {
         //====================================================================//
-        // Field is not an Attribute
-        $attr = $this->isAttribute($fieldName);
-        if (is_null($attr) || !isset($this->contactData)) {
+        // Field is not a Property
+        if (!$attr = $this->isProperty($fieldName)) {
             return;
         }
-        /**
-         *====================================================================//
-         * Extract Original Attribute Value
-         *
-         * @codingStandardsIgnoreStart
-         */
-        $origin = $this->getAttributeValue($attr->Name, $attr->Datatype);
         //====================================================================//
-        // No Changes
-        if ($origin == $fieldData) {
-            unset($this->in[$fieldName]);
+        // Ensure Properties are Loaded
+        $manager = $this->connector->getLocator()->getPropertiesManager();
+        $manager->loadContactProperties($this->object);
+        //====================================================================//
+        // Transform Splash Value to Mailjet Value
+        $mailjetValue = PropertyTransformer::toMailjet($attr, $fieldData);
+        //====================================================================//
+        // Compare & Update Property Value
+        $origin = $this->object->properties[$fieldName] ?? null;
+        if ($origin != $mailjetValue) {
+            $this->object->properties[$fieldName] = $mailjetValue;
+            $this->visitor->getExtendedActionsBuffer()->add(
+                ExtendedActionsTypes::POST_UPDATE,
+                $this->buildPropertyMutation($fieldName, $mailjetValue)
+            );
+            $this->needUpdate();
+        }
 
-            return;
-        }
-        //====================================================================//
-        // Update Attribute Value
-        $this->setAttributeValue($attr->Name, $fieldData);
-        // @codingStandardsIgnoreEnd
         unset($this->in[$fieldName]);
     }
 
+    //====================================================================//
+    // PRIVATE METHODS
+    //====================================================================//
+
     /**
-     * Read requested Field Data
-     *
-     * @param string $name   Input List Key
-     * @param string $format Field Identifier / Name
-     *
-     * @throws Exception
-     *
-     * @return null|bool|string
+     * Check if this Field is a Property
      */
-    private function getAttributeValue(string $name, string $format): bool|string|null
+    private function isProperty(string $fieldName): ?stdClass
     {
-        //====================================================================//
-        // Safety Check => Attributes Are Iterable
-        if (!is_iterable($this->contactData)) {
-            return null;
-        }
-        //====================================================================//
-        // Walk on Member Attributes
-        foreach ($this->contactData as $attrValue) {
-            //====================================================================//
-            // Search Requested Attribute
-            // @codingStandardsIgnoreStart
-            if ($attrValue->Name != $name) {
-                continue;
-            }
-            //====================================================================//
-            // Extract Attribute Value
-            switch ($format) {
-                case 'bool':
-                    return ("true" == $attrValue->Value);
-                case 'datetime':
-                    if (empty($attrValue->Value)) {
-                        return false;
-                    }
-                    $date = new DateTime($attrValue->Value);
-
-                    return $date->format(SPL_T_DATETIMECAST);
-                default:
-                    return $attrValue->Value;
-            }
-            // @codingStandardsIgnoreEnd
-        }
-
-        return null;
+        return $this->connector
+            ->getLocator()
+            ->getPropertiesManager()
+            ->findByFieldName($fieldName)
+        ;
     }
 
     /**
-     * Write Requested Attribute Data
-     *
-     * @param string      $name      Input List Key
-     * @param null|string $fieldData Field Data
-     *
-     * @return void
+     * Build a Mutation for a Contact Property Update
      */
-    private function setAttributeValue(string $name, ?string $fieldData): void
+    private function buildPropertyMutation(string $fieldName, ?string $mailjetValue): Mutation
     {
-        //====================================================================//
-        // Safety Check => Attributes Are Iterable
-        if (!is_iterable($this->contactData)) {
-            return;
-        }
-        //====================================================================//
-        // Prepare New Attribute Value
-        $newAttr = new stdClass();
-        // @codingStandardsIgnoreStart
-        $newAttr->Name = $name;
-        $newAttr->Value = is_null($fieldData) ? "" : (string) $fieldData;
-        //====================================================================//
-        // Walk on Member Attributes
-        foreach ($this->contactData as $index => $attrValue) {
-            //====================================================================//
-            // Search Requested Attribute
-            if ($attrValue->Name != $name) {
-                // @codingStandardsIgnoreEnd
-                continue;
-            }
-            //====================================================================//
-            // Update Attribute Value
-            $this->contactData[$index] = $newAttr;
-            $this->needUpdate("contactData");
-
-            return;
-        }
-
-        //====================================================================//
-        // Add Attribute Value
-        $this->contactData[] = $newAttr;
-        $this->needUpdate("contactData");
-    }
-
-    /**
-     * Check if this Attribute Exists
-     *
-     * @param string $fieldName
-     *
-     * @return null|stdClass
-     */
-    private function isAttribute(string $fieldName) : ?stdClass
-    {
-        //====================================================================//
-        // Safety Check => Attributes Are Loaded
-        if (empty($this->attrCache)) {
-            $attributes = $this->getParameter("MembersAttributes");
-            if (empty($attributes) || !is_array($attributes)) {
-                return null;
-            }
-            $this->attrCache = $attributes;
-        }
-
-        foreach ($this->attrCache as $attr) {
-            // @codingStandardsIgnoreStart
-            if ($fieldName == strtolower($attr->Name)) {
-                return $attr;
-            }
-            // @codingStandardsIgnoreEnd
-        }
-
-        return null;
-    }
-
-    /**
-     * Get Splash Attribute Type Name
-     *
-     * @param stdClass $attribute
-     *
-     * @return string
-     */
-    private static function toSplashType(stdClass $attribute): string
-    {
-        //====================================================================//
-        // From mapping
-        // @codingStandardsIgnoreStart
-        if (isset(self::$attrType[$attribute->Datatype])) {
-            return self::$attrType[$attribute->Datatype];
-        }
-
-        // @codingStandardsIgnoreEnd
-        //====================================================================//
-        // Default Type
-        return SPL_T_VARCHAR;
+        return Mutation::update(
+            Contact::class,
+            sprintf("/contactdata/%s", $this->object->getId()),
+            $this->object->getId(),
+        )->withData(array("Data" => array(array(
+            "Name" => $fieldName,
+            "Value" => $mailjetValue,
+        ))));
     }
 }
---
+++ src/Services/Managers/PropertiesManager.php
@@ -0,0 +1,199 @@
+<?php
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Splash\Connectors\Mailjet\Services\Managers;
+
+use Splash\Connectors\Mailjet\Helpers\PropertiesHelper;
+use Splash\Connectors\Mailjet\Models\Api\Contact;
+use Splash\Connectors\Mailjet\Models\MailjetConnectorAwareTrait;
+use Splash\Core\Components\FieldsFactory;
+use stdClass;
+
+/**
+ * Manage Mailjet Contact Metadata (Custom Properties)
+ */
+class PropertiesManager
+{
+    use MailjetConnectorAwareTrait;
+
+    /**
+     * Contact Properties Details Storage Key
+     */
+    const string PROPERTIES_DETAILS = "ApiContactPropertiesDetails";
+
+    /**
+     * Base Attributes Metadata Item Type
+     */
+    const string ITEM_TYPE = "http://meta.schema.org/additionalType";
+
+    /**
+     * Fetch Contact Metadata Properties from Mailjet API
+     */
+    public function fetchContactProperties(): bool
+    {
+        //====================================================================//
+        // Get Contact Metadata from Api
+        $response = $this->getConnexion()->get('/contactmetadata');
+        if (is_null($response) || empty($response["Data"]) || !is_array($response["Data"])) {
+            return false;
+        }
+
+        //====================================================================//
+        // Filter to keep only static properties
+        $propertiesDetails = array_values(array_filter(
+            $response["Data"],
+            static fn (array $property) => self::isStaticProperty($property)
+        ));
+        //====================================================================//
+        // Store in Connector Settings
+        $this->getConnector()->setParameter(
+            self::PROPERTIES_DETAILS,
+            json_decode((string) json_encode($propertiesDetails))
+        );
+        //====================================================================//
+        // Update Connector Settings
+        $this->getConnector()->updateConfiguration();
+
+        return true;
+    }
+
+    /**
+     * Load Contact Properties from API if not already loaded
+     *
+     * @return null|array Contact properties data, null on failure
+     */
+    public function loadContactProperties(Contact $contact): ?array
+    {
+        //====================================================================//
+        // Already Loaded
+        if (null !== $contact->properties) {
+            return $contact->properties;
+        }
+        //====================================================================//
+        // Load Contact Properties from API
+        $response = $this->getConnexion()->get(
+            sprintf('/contactdata/%d', $contact->ID)
+        );
+        if (is_null($response) || empty($response["Data"]) || !is_array($response["Data"])) {
+            return null;
+        }
+        //====================================================================//
+        // Parse Properties as Key => Value
+        $properties = array();
+        foreach ($response["Data"][0]["Data"] ?? array() as $property) {
+            if (is_array($property) && isset($property["Name"])) {
+                $properties[strtolower($property["Name"])] = $property["Value"] ?? null;
+            }
+        }
+        //====================================================================//
+        // Store Properties on Contact
+        $contact->properties = $properties;
+
+        return $contact->properties;
+    }
+
+    /**
+     * Find a Property by its Field Name
+     */
+    public function findByFieldName(string $fieldName): ?stdClass
+    {
+        //====================================================================//
+        // Walk on Contact Properties
+        foreach ($this->getPropertiesDetails() as $attr) {
+            if (strtolower($attr->Name) == $fieldName) {
+                return $attr;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Build Fields using FieldFactory
+     */
+    public function buildPropertiesFields(FieldsFactory $factory): void
+    {
+        //====================================================================//
+        // Create Properties Fields
+        foreach ($this->getPropertiesDetails() as $attr) {
+            $this->buildPropertyField($factory, $attr);
+        }
+    }
+
+    //====================================================================//
+    // PRIVATE METHODS
+    //====================================================================//
+
+    /**
+     * Get Properties Details as stdClass array (safe after serialization)
+     *
+     * @return stdClass[]
+     */
+    private function getPropertiesDetails(): array
+    {
+        $raw = $this->getConnector()->getParameter(self::PROPERTIES_DETAILS);
+        if (empty($raw) || !is_array($raw)) {
+            return array();
+        }
+        //====================================================================//
+        // Ensure stdClass format (serialization may convert to arrays)
+        return array_filter(array_map(
+            static fn ($item) => $item instanceof stdClass
+                ? $item
+                : (is_array($item) ? (object) $item : null),
+            $raw
+        ));
+    }
+
+    /**
+     * Check if a property is static (user-defined) and should be managed.
+     *
+     * @param array<string, mixed>|stdClass $property
+     */
+    private static function isStaticProperty(array|stdClass $property): bool
+    {
+        $nameSpace = is_array($property)
+            ? ($property["NameSpace"] ?? "")
+            : ($property->NameSpace ?? "")
+        ;
+
+        return empty($nameSpace) || "static" === $nameSpace;
+    }
+
+    /**
+     * Build Field using FieldFactory
+     */
+    private function buildPropertyField(FieldsFactory $factory, stdClass $attr): void
+    {
+        $attrCode = strtolower($attr->Name);
+        //====================================================================//
+        // Check for Known Field Template
+        if ($template = PropertiesHelper::getTemplate($attr)) {
+            $factory->createFromTemplate($attrCode, $template);
+        } else {
+            //====================================================================//
+            // Add Attribute to Fields
+            $factory
+                ->create(PropertiesHelper::toSplashType($attr))
+                ->identifier($attrCode)
+                ->name($attr->Name)
+                ->microData(self::ITEM_TYPE, $attrCode)
+            ;
+        }
+        //====================================================================//
+        // Configure Field
+        $factory->group("Attributes");
+    }
+}
